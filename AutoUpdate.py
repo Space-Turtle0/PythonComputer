c19c86d6de44c7b1b6f5a58b3243f01fd559c738
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2020-05-13 14:51:40 -0400

    Update AutoUpdate.py

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -1,6 +1,8 @@
 import ghau
-
+import time
 VERSION = "v4.1.3"
 REPO = "Space-Turtle0/PythonComputer"
 update = ghau.Update(version=VERSION, repo=REPO, reboot=ghau.python("2019pc.py"))
 update.cl_test()
+print("Complete...")
+time.sleep(2)
