1592db925979ef7d773203d8f457fddb887b95e9
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2020-05-14 19:49:17 -0400

    Update AutoUpdate.py

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -1,12 +1,8 @@
-import os
-import time
 import ghau
-VERSION = "v4.2"
+import time
+VERSION = "v4.2.4"
 REPO = "Space-Turtle0/PythonComputer"
-if "GAPI" in os.envrion:
-  update = ghau.Update(version=VERSION, repo=REPO, reboot=ghau.python("2019pc.py"), auth=os.environ["GAPI"])
-else:
-  update = ghau.Update(version=VERSION, repo=REPO, reboot=ghau.python("2019pc.py"))
+update = ghau.Update(version=VERSION, repo=REPO, reboot=ghau.python("2019pc.py"))
 update.update()
 print("Complete...")
 time.sleep(2)
