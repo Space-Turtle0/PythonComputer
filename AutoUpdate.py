7925928550f284617cc6ab15616ea2c0c8e37045
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2020-05-14 13:50:19 -0400

    Version Update
    Version is now v4.2

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -1,6 +1,6 @@
 import ghau
 import time
-VERSION = "v4.1.7"
+VERSION = "v4.2"
 REPO = "Space-Turtle0/PythonComputer"
 update = ghau.Update(version=VERSION, repo=REPO, reboot=ghau.python("2019pc.py"))
 update.update()
